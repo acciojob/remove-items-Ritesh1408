0f1a20fe68ef228fcc6a657f5ff0e9447d570529
RITESH ROUSHAN KUMAR <78962046+Ritesh1408@users.noreply.github.com>
2025-03-02 13:57:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-//your JS code here. If required.
+let select = document.getElementById("colorSelect");
+let button = document.querySelector("input[type='button']");
+
+button.addEventListener("click", function (){
+    let selectedOption = select.options[select.selectedIndex];
+    if (selectedOption) {
+        selectedOption.remove();
+    }
+});
